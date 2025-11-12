785421f6c962f6606fd852829e57f21665d5d9a5
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2025-11-12 08:47:50 +0100

    --farcaster

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
-
-import App from "./grant";
-
+'use client';
+import { useEffect } from 'react';
+import { sdk } from '@farcaster/miniapp-sdk';
+import App from './grant';
 
 export default function Home() {
+  useEffect(() => {
+    const init = async () => {
+      await sdk.actions.ready();
+      console.log('Farcaster Mini App is ready!');
+    };
+    init();
+  }, []);
+
   return (
     <main className="">
       <App />
