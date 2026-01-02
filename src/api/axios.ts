1cd15acaa7234f8bcacf17dab4ac232fa38bc57e
0x3devoid <owoyemiidrisolamilekan123@gmail.com>
2026-01-02 12:57:59 -0800

    signup && signIn reform

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const server = "https://mintfiat.onrender.com";
+const server = "https://mintfiat-db.onrender.com";
 const local = "http://localhost:5000"
 
 export const api = axios.create({
-  baseURL: local,
+  baseURL: server,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
