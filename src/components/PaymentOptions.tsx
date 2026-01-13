eb03b4bf24bb30617967e63a7c59d6f45d089968
0x3devoid <owoyemiidrisolamilekan123@gmail.com>
2026-01-13 02:14:15 -0800

    paypal --payemt

--- src/components/PaymentOptions.tsx
+++ src/components/PaymentOptions.tsx
@@ -1,104 +1,90 @@
-import React from 'react'
-import { Send, Rocket, X, Gift, ArrowUpRight, ArrowDownRight, Coins, Plus, Minus, ArrowBigRight, ChevronRight } from 'lucide-react';
-import Image from 'next/image';
-const PaymentOptions = () => {
-    return (
-        <div className='bg-[#1b1d22] p-2 mt-5 rounded-xl'>
+import React, { useState } from "react";
+import {
+  Send,
+  Rocket,
+  X,
+  Gift,
+  ArrowUpRight,
+  ArrowDownRight,
+  Coins,
+  Plus,
+  Minus,
+  ArrowBigRight,
+  ChevronRight,
+} from "lucide-react";
+import Image from "next/image";
+import { api } from "@/api/axios";
+import axios from "axios";
+import { useAuth } from "@/context/AuthContext";
+import CenterLoader from "@/utils/loader";
+import { notify } from "@/utils/notify";
 
-            <div className='flex justify-between items-center'>
-                <div className='flex space-x-3'>
-                    <div className="w-10 h-10 flex items-center justify-center ">
-                        <Image
-                            src="/images/paypal.png"
-                            alt="PayPal"
-                            width={40}
-                            height={40}
-                            className="object-contain rounded"
-                        />
-                    </div>
-
-                    <div className='flex flex-col'>
-                        <h3 className='font-bold'>PayPal</h3>
-
-                    </div>
-                </div>
-
-                <ChevronRight className="w-5 h-5 text-white" />
-            </div>
-
-            <div className='flex justify-between items-center mt-2'>
-                <div className='flex space-x-3'>
-                    <div className="w-10 h-10 flex items-center justify-center ">
-                        <Image
-                            src="/images/opay.png"
-                            alt="p"
-                            width={40}
-                            height={40}
-                            className="object-contain rounded"
-                        />
-                    </div>
-
-
-
-                    <div className='flex flex-col'>
-                        <h3 className='font-bold'>Opay</h3>
-                        <p className='text-[8px] text-gray-400'>
-
-                        </p>
-
-                    </div>
-                </div>
-
-                <ChevronRight className="w-5 h-5 text-white" />
-            </div>
-
-            <div className='flex justify-between items-center mt-2'>
-                <div className='flex space-x-3'>
-                    <div className="w-10 h-10 flex items-center justify-center ">
-                        <Image
-                            src="/images/cashapp.png"
-                            alt="p"
-                            width={40}
-                            height={40}
-                            className="object-contain rounded"
-                        />
-                    </div>
-
-                    <div className='flex flex-col'>
-                        <h3 className='font-bold'>Cash App</h3>
-                        <p className='text-[8px] text-gray-400'>
-
-                        </p>
-
-                    </div>
-                </div>
-
-                <ChevronRight className="w-5 h-5 text-white" />
-            </div>
-
-            <div className='flex justify-between items-center mt-2'>
-                <div className='flex space-x-3'>
-                    <div className="w-10 h-10 flex items-center justify-center ">
-                        <Image
-                            src="/images/revolut.png"
-                            alt="p"
-                            width={40}
-                            height={40}
-                            className="object-contain rounded"
-                        />
-                    </div>
-
-                    <h3 className='font-bold'>Revolut</h3>
-                </div>
-
-                <ChevronRight className="w-5 h-5 text-white" />
-            </div>
-
-
-
-
-        </div>
-    )
+interface PaymentOpt {
+  type: "paypal";
 }
 
-export default PaymentOptions
+const PaymentOptions = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  async function handleDeposit(opt: PaymentOpt) {
+    try {
+      if (opt.type === "paypal") {
+        setLoading(true);
+        await createPaypalOrder();
+      }
+    } catch (error: any) {
+      let message = "Something went wrong. Please try again.";
+      console.log(error)
+
+      if (axios.isAxiosError(error)) {
+        message = error.response?.data?.message || error.message || message;
+      }
+      notify(message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function createPaypalOrder() {
+    for (let i = 0; i < 3; i++) {
+      try {
+        const data = { amount: 300, currency: "USD" };
+        return await api.post("/auth/paypal/order/create", data);
+      } catch (err: any) {
+        if (i === 2) throw err;
+        await new Promise((r) => setTimeout(r, 1000));
+      }
+    }
+  }
+  return (
+    <>
+      <CenterLoader show={loading} />
+      <div className="bg-[#1b1d22] p-2 mt-5 rounded-xl">
+        <div
+          className="flex justify-between items-center cursor-pointer"
+          onClick={() => handleDeposit({ type: "paypal" })}
+        >
+          <div className="flex space-x-3 ">
+            <div className="w-10 h-10 flex items-center justify-center ">
+              <Image
+                src="/images/paypal.png"
+                alt="PayPal"
+                width={40}
+                height={40}
+                className="object-contain rounded"
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <h3 className="font-bold">PayPal</h3>
+            </div>
+          </div>
+
+          <ChevronRight className="w-5 h-5 text-white" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PaymentOptions;
