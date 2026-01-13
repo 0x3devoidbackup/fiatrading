2145be3f7e510fc5bcb329bbc1d0b13cbd1d5a9a
0x3devoid <owoyemiidrisolamilekan123@gmail.com>
2026-01-13 12:07:07 -0800

    paypal --payemt

--- src/app/send/page.tsx
+++ src/app/send/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { ArrowLeft } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/context/AuthContext";
@@ -8,17 +8,34 @@ import { api } from "@/api/axios";
 import CenterLoader from "@/utils/loader";
 import { notify } from "@/utils/notify";
 import axios from "axios";
+import { X } from "lucide-react";
 
 export default function SendPage() {
   const router = useRouter();
-  const { user } = useAuth();
+  const { user, checkAuth } = useAuth();
 
   const [currency, setCurrency] = useState<FiatCurrency | "">("");
   const [quantity, setQuantity] = useState<string>("");
   const [recipientUID, setRecipientUID] = useState<string>("");
 
+  const inputsRef = useRef<(HTMLInputElement | null)[]>([]);
   const [available, setAvailable] = useState<number>(0);
   const [loading, setLoading] = useState(false);
+  const [otpContinue, setOtpContinuation] = useState<boolean>(false);
+
+  const [code, setCode] = useState(["", "", "", "", "", ""]);
+
+  const handleCodeChange = (value: string, index: number) => {
+    if (!/^\d?$/.test(value)) return;
+
+    const newCode = [...code];
+    newCode[index] = value;
+    setCode(newCode);
+
+    if (value && index < 5) {
+      inputsRef.current[index + 1]?.focus();
+    }
+  };
 
   // Update available balance whenever user or currency changes
   useEffect(() => {
@@ -44,7 +61,7 @@ export default function SendPage() {
 
   // Submit transaction
   const handleSubmit = async () => {
-    const amount = Number(quantity); // convert here
+    const amount = Number(quantity);
 
     if (!currency || !amount || !recipientUID) {
       notify("Please fill all fields");
@@ -65,21 +82,68 @@ export default function SendPage() {
       };
 
       const res = await api.post("/users/assets/send/fiat", payload);
-      notify("Transaction successful!");
-      console.log(res.data);
-
-      // Reset form
-      setQuantity("");
-      setRecipientUID("");
-      setCurrency("");
+      console.log(res);
+      if (res.status === 201 || res.data.otpSendingStatus.status === 200) {
+        setOtpContinuation(true);
+      }
     } catch (error: any) {
       let message = "Something went wrong. Please try again.";
-      console.log(error)
+      console.log(error);
 
       if (axios.isAxiosError(error)) {
-        message = error.response?.data?.message || error.response?.data?.errors[0].path + ": " + error.response?.data?.errors[0].msg || message;
+        message =
+          error.response?.data?.message ||
+          error.response?.data?.errors[0].path +
+            ": " +
+            error.response?.data?.errors[0].msg ||
+          message;
       }
       notify(message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleCodeContinuation = async (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+
+    const isComplete =
+      Array.isArray(code) &&
+      code.length === 6 &&
+      code.every((digit) => digit !== "");
+
+    if (!isComplete) {
+      notify("OTP code is incomplete");
+      return;
+    }
+    const otpCode = code.join("");
+    try {
+      setLoading(true);
+      const data = {
+        otp: otpCode,
+        email: user?.email
+      };
+      const response = await api.post("/auth/verify-otp/transaction", data);
+      console.log(response);
+      if (response.status === 200) {
+        notify("Transaction successful")
+        setQuantity("");
+        setRecipientUID("");
+        setCurrency("");
+        setOtpContinuation(false)
+        await checkAuth();
+        router.push("/portfolio")
+      }
+    } catch (error: any) {
+      let message = "Something went wrong. Please try again.";
+
+      if (axios.isAxiosError(error)) {
+        message = error.response?.data?.message || error.message || message;
+      }
+      notify(message);
+      return false;
     } finally {
       setLoading(false);
     }
@@ -172,6 +236,72 @@ export default function SendPage() {
           {loading ? "Sending..." : "Submit"}
         </button>
       </div>
+
+      {otpContinue && (
+        <div className="fixed inset-0 bg-[#0a0a0a] z-50">
+          <div className="max-w-md mx-auto px-4 pb-40 mt-5">
+            <div className="flex items-center justify-end mb-4">
+              <X
+                className="w-6 h-6 font-bold text-neutral-300 cursor-pointer"
+                onClick={() => setOtpContinuation(false)}
+              />
+            </div>
+            <CenterLoader show={loading} />
+
+            <h2 className="text-xl font-bold">You're Almost There</h2>
+
+            <p className="text-sm mt-3 opacity-70 mb-1">
+              Enter the 6-digit verification code for the email
+            </p>
+
+            <p className="text-sm font-semibold mb-8">
+              {user?.email}
+              <span className="opacity-60">(valid for 15 minutes)</span>
+            </p>
+
+            <form onSubmit={handleCodeContinuation}>
+              <div className="flex items-center justify-between m-auto mb-4 max-w-sm">
+                {code.map((digit, i) => (
+                  <input
+                    key={i}
+                    ref={(el) => {
+                      inputsRef.current[i] = el;
+                    }}
+                    id={`otp-${i}`}
+                    type="text"
+                    value={digit}
+                    maxLength={1}
+                    onChange={(e) => handleCodeChange(e.target.value, i)}
+                    className="w-12 h-12 text-center text-xl font-semibold bg-[#111] border border-[#222] rounded-xl focus:outline-none focus:border-blue-500"
+                  />
+                ))}
+              </div>
+
+              <div className="mb-10 text-sm">
+                {/* <p className="opacity-50 ">
+                  Get code again after <span className="text-white">7s</span>
+                </p> */}
+                <button type="button" className="text-[#9DD1F1] cursor-pointer">
+                  Request Again
+                </button>
+              </div>
+
+              <div className="px-5 pb-6">
+                <button
+                  type="submit"
+                  className="w-full  cursor-pointer bg-blue-600 py-2 rounded-full font-semibold text-sm"
+                >
+                  Next
+                </button>
+
+                <p className="text-center mt-5 text-blue-600 text-sm">
+                  Didn't Receive Verification Code ?
+                </p>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
